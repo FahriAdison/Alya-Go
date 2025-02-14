3c8574e1778fa0b1ea6029fa539189b4f52cdc29
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2025-02-14 08:27:13 +0700

    Fix : Wrong Syntax

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,40 +1,34 @@
 package plugins
 
 import (
-    "go.mau.fi/whatsmeow"
-    "go.mau.fi/whatsmeow/types"
-    "go.mau.fi/whatsmeow/types/events"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/types/events"
 )
 
-// Plugin interface
 type Plugin interface {
-    Name() string
-    Commands() []string
-    Handle(client *whatsmeow.Client, msg *events.Message)
+	Name() string
+	Commands() []string
+	Handle(client *whatsmeow.Client, msg *events.Message)
 }
 
-// Registered plugins
 var registeredPlugins []Plugin
 
-// Register a plugin
 func Register(plugin Plugin) {
-    registeredPlugins = append(registeredPlugins, plugin)
+	registeredPlugins = append(registeredPlugins, plugin)
 }
 
-// Handle incoming messages
 func HandleMessage(client *whatsmeow.Client, msg *events.Message) {
-    if msg.Info.IsFromMe || msg.Message.GetConversation() == "" {
-	return
-    }
-
-    // Check for commands
-    text := msg.Message.GetConversation()
-    for _, plugin := range registeredPlugins {
-	for _, cmd := range plugin.Commands() {
-	    if text == cmd {
-		plugin.Handle(client, msg)
+	if msg.Info.IsFromMe || msg.Message.GetConversation() == "" {
 		return
-	    }
 	}
-    }
+
+	text := msg.Message.GetConversation()
+	for _, plugin := range registeredPlugins {
+		for _, cmd := range plugin.Commands() {
+			if text == cmd {
+				plugin.Handle(client, msg)
+				return
+			}
+		}
+	}
 }
