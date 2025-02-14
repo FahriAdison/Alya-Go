6748204983a2489e3cf97406f83ab358ceb478c2
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2025-02-14 08:13:11 +0700

    Add : Feature Plugins

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,1 +1,40 @@
+package plugins
 
+import (
+    "go.mau.fi/whatsmeow"
+    "go.mau.fi/whatsmeow/types"
+    "go.mau.fi/whatsmeow/types/events"
+)
+
+// Plugin interface
+type Plugin interface {
+    Name() string
+    Commands() []string
+    Handle(client *whatsmeow.Client, msg *events.Message)
+}
+
+// Registered plugins
+var registeredPlugins []Plugin
+
+// Register a plugin
+func Register(plugin Plugin) {
+    registeredPlugins = append(registeredPlugins, plugin)
+}
+
+// Handle incoming messages
+func HandleMessage(client *whatsmeow.Client, msg *events.Message) {
+    if msg.Info.IsFromMe || msg.Message.GetConversation() == "" {
+	return
+    }
+
+    // Check for commands
+    text := msg.Message.GetConversation()
+    for _, plugin := range registeredPlugins {
+	for _, cmd := range plugin.Commands() {
+	    if text == cmd {
+		plugin.Handle(client, msg)
+		return
+	    }
+	}
+    }
+}
