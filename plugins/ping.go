392f9fc4579c0f5e9968289593ab9e346d4140b5
Papah-Chan <92633211+FahriAdison@users.noreply.github.com>
2025-02-14 08:26:03 +0700

    Fix : Wrong Syntax

--- plugins/ping.go
+++ plugins/ping.go
@@ -1,32 +1,30 @@
 package plugins
 
 import (
-    "context"
-    "go.mau.fi/whatsmeow"
-    "go.mau.fi/whatsmeow/types/events"
+	"context"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/types/events"
 )
 
-// Ping plugin
 type PingPlugin struct{}
 
 func (p *PingPlugin) Name() string {
-    return "Ping"
+	return "Ping"
 }
 
 func (p *PingPlugin) Commands() []string {
-    return []string{"!ping"}
+	return []string{"!ping"}
 }
 
 func (p *PingPlugin) Handle(client *whatsmeow.Client, msg *events.Message) {
-    _, err := client.SendMessage(context.Background(), msg.Info.Chat, &whatsmeow.TextMessage{
-	Content: "🏓 Pong!",
-    })
-    if err != nil {
-	println("Failed to send ping response:", err.Error())
-    }
+	_, err := client.SendMessage(context.Background(), msg.Info.Chat, &whatsmeow.TextMessage{
+		Content: "🏓 Pong!",
+	})
+	if err != nil {
+		println("Failed to send ping response:", err.Error())
+	}
 }
 
-// Register plugin
 func init() {
-    Register(&PingPlugin{})
+	Register(&PingPlugin{})
 }
